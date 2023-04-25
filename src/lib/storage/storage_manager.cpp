0ca2f44eee859cec17c84df1320719d210fd4b9e
Magnus Menger <magnus@magnusmenger.de>
2023-04-25 16:10:10 +0200

    implement storage manager

--- src/lib/storage/storage_manager.cpp
+++ src/lib/storage/storage_manager.cpp
@@ -5,42 +5,46 @@
 namespace opossum {
 
 StorageManager& StorageManager::get() {
-  return *(new StorageManager());
-  // A really hacky fix to get the tests to run - replace this with your implementation
+  static StorageManager instance;
+  return instance;
 }
 
 void StorageManager::add_table(const std::string& name, std::shared_ptr<Table> table) {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  Assert(!has_table(name), "Tried to create table with existing name.");
+  _tables[name] = table;
 }
 
 void StorageManager::drop_table(const std::string& name) {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  Assert(has_table(name), "Tried to drop a non existing table.");
+  _tables.erase(name);
 }
 
 std::shared_ptr<Table> StorageManager::get_table(const std::string& name) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  Assert(has_table(name), "Tried to get a non existing table.");
+  return _tables.at(name);
 }
 
 bool StorageManager::has_table(const std::string& name) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  return _tables.contains(name);
 }
 
 std::vector<std::string> StorageManager::table_names() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  auto table_names = std::vector<std::string>();
+  table_names.reserve(_tables.size());
+  for (const auto& [name, table] : _tables) {
+    table_names.emplace_back(name);
+  }
+  return table_names;
 }
 
 void StorageManager::print(std::ostream& out) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  for (const auto& [name, table] : _tables) {
+    out << "(\"" << name << "\", " << table->column_count() << " columns, " <<  table->row_count() << " rows, " << table->chunk_count() << " chunks)" << std::endl;
+  }
 }
 
 void StorageManager::reset() {
-  // Implementation goes here
+  _tables.clear();
 }
 
 }  // namespace opossum
